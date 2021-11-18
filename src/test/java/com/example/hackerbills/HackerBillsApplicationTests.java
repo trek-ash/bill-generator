010f0467d279b65fa9f7c3edf8a6cf49d44a88a3
Yash <yash@Yashs-MacBook-Air.local>
2021-11-18 20:55:03 +0530

    Test initialised

---
+++ src/test/java/com/example/hackerbills/HackerBillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackerbills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerBillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
